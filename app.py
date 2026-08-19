355b8f9cb022ae4a80087b481d25b2802f871545
Isu Paul <isu.paul.pi@gmail.com>
2026-08-19 16:55:17 +0300

    v3
    New version for vma update

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ st.write("Input the parameters below to predict Surface Resistivity (SR).")
 
 # Create input fields for each feature in your model
 days = st.number_input("Days", min_value=1.0, max_value=100.0, value=28.0)
-graphene = st.number_input("Graphene Content", min_value=0.0, max_value=10.0, value=0.1)
+graphene = st.number_input("Graphene Content", min_value=0.0, max_value=5.0, value=0.1)
 wc = st.number_input("W/C Ratio", min_value=0.2, max_value=0.5, value=0.3)
 sand_c = st.number_input("Sand/C", min_value=1.0, max_value=2.0, value=1.429)
 sp_c = st.number_input("SP/C", min_value=0.0, max_value=0.1, value=0.004)
-sta_c = st.number_input("Sta/C", min_value=0.0, max_value=0.1, value=0.001)
+sta_c = st.number_input("VMA/C", min_value=0.0, max_value=0.1, value=0.001)
 upv = st.number_input("UPV", min_value=2.0, max_value=5.0, value=3.5)
 
 if st.button("Predict Strength"):
@@ -24,7 +24,7 @@ if st.button("Predict Strength"):
         
         # Create a dataframe from inputs matching the training feature order
         input_data = pd.DataFrame([[days, graphene, wc, sand_c, sp_c, sta_c, upv]],
-                                  columns=['Days', 'GNP', 'W/C', 'Sand/C', 'SP/C', 'VMA/C', 'UPV'])
+                                  columns=['Days', 'Graphene', 'W/C', 'Sand/C', 'SP/C', 'VMA/C', 'UPV'])
         
         # Perform prediction
         prediction = model.predict(input_data)
